7cc5fd97693f299a9d25e3e0587a90d63d321da8
weitzly@gmail.com <w3Itzman>
2020-07-17 12:14:28 -0700

    replace curl wiht rsync

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 echo "Running deployment script..."
-for file in $BUILD_PATH/*
-do
-  curl -k "sftp://$HOST/$REMOTE_PATH/$BUILD_PATH/${file}" --user "$FTP_USR:$FTP_PASS" -T ${file} --ftp-create-dirs
-done
+# sftl diabled in curl!
+# for file in $BUILD_PATH/*
+# do
+#   curl -k "sftp://$HOST/$REMOTE_PATH/$BUILD_PATH/${file}" --user "$FTP_USR:$FTP_PASS" -T ${file} --ftp-create-dirs
+# done
+# use rsync
+rsync -a $BUILD_PATH $FTP_USER@$REMOTE_IP:$REMOTE_PATH/$BUILD_PATH
 echo "Pushed deployment successfully"
 exit 0
