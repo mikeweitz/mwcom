de5d7849a0e00c5af414cd2c28938332d0cae870
weitzly@gmail.com <w3Itzman>
2020-07-26 22:12:53 -0700

    update restart script

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -2,10 +2,7 @@
 ssh "$FTP_USER"@"$REMOTE_IP" << "EOF"
   echo 'entering remote server'
   pwd
-  cd /var/www/weitzly.com
-  ls -la
-  echo '^^^^^^^ Should look familiar ^^^^^^^'
-  pm2 restart nextjs
+  pm2 restart nextjs && echo "nice nice"
   echo "$FTP_PASS" | service nginx restart
 EOF
 exit 0;
