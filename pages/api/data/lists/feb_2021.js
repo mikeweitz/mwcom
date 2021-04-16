048ec4832c1fb4fb74263334ee4a223cd1343213
weitzly@gmail.com <w3Itzman>
2021-04-16 15:52:28 -0700

    add jan / feb playlists

---
+++ pages/api/data/lists/feb_2021.js
@@ -0,0 +1,130 @@
+export const data = {
+  collaborative: false,
+  description: '',
+  followers: {
+    total: 0,
+  },
+  images: [
+    {
+      height: 640,
+      url:
+        'https://mosaic.scdn.co/640/ab67616d0000b2732d0fb302fc70db48c971b3dcab67616d0000b273302f974c7680d0e72d8e70dbab67616d0000b27371b470e369fd5b675422b8cbab67616d0000b273dec2c44468f6b679c1db54f3',
+      width: 640,
+    },
+    {
+      height: 300,
+      url:
+        'https://mosaic.scdn.co/300/ab67616d0000b2732d0fb302fc70db48c971b3dcab67616d0000b273302f974c7680d0e72d8e70dbab67616d0000b27371b470e369fd5b675422b8cbab67616d0000b273dec2c44468f6b679c1db54f3',
+      width: 300,
+    },
+    {
+      height: 60,
+      url:
+        'https://mosaic.scdn.co/60/ab67616d0000b2732d0fb302fc70db48c971b3dcab67616d0000b273302f974c7680d0e72d8e70dbab67616d0000b27371b470e369fd5b675422b8cbab67616d0000b273dec2c44468f6b679c1db54f3',
+      width: 60,
+    },
+  ],
+  name: 'February 2021',
+  owner: {
+    display_name: 'Weity',
+    id: '1247970817',
+    uri: 'spotify:user:1247970817',
+  },
+  public: false,
+  tracks: {
+    items: [
+      {
+        track: {
+          duration_ms: 356853,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 172440,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 214360,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 172300,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 265835,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 165440,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 219029,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 141000,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 199480,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 229840,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 253266,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 200471,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 262542,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 129981,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 193397,
+          type: 'track',
+        },
+      },
+    ],
+    total: 15,
+  },
+  uri: 'spotify:playlist:1VmRBxgkuAvnKKSrleI5j6',
+};
---
+++ pages/api/data/lists/jan_2021.js
@@ -0,0 +1,124 @@
+export const data = {
+  collaborative: false,
+  description: '',
+  followers: {
+    total: 0,
+  },
+  images: [
+    {
+      height: 640,
+      url:
+        'https://mosaic.scdn.co/640/ab67616d0000b27316ff5471107eda777437aa81ab67616d0000b2737c9dbb7ec1e865198386a822ab67616d0000b2738d7d3584e5220d0cd406eea4ab67616d0000b273a6370891414f39e41894af2b',
+      width: 640,
+    },
+    {
+      height: 300,
+      url:
+        'https://mosaic.scdn.co/300/ab67616d0000b27316ff5471107eda777437aa81ab67616d0000b2737c9dbb7ec1e865198386a822ab67616d0000b2738d7d3584e5220d0cd406eea4ab67616d0000b273a6370891414f39e41894af2b',
+      width: 300,
+    },
+    {
+      height: 60,
+      url:
+        'https://mosaic.scdn.co/60/ab67616d0000b27316ff5471107eda777437aa81ab67616d0000b2737c9dbb7ec1e865198386a822ab67616d0000b2738d7d3584e5220d0cd406eea4ab67616d0000b273a6370891414f39e41894af2b',
+      width: 60,
+    },
+  ],
+  name: 'January 2021',
+  owner: {
+    display_name: 'Weity',
+    id: '1247970817',
+    uri: 'spotify:user:1247970817',
+  },
+  public: false,
+  tracks: {
+    items: [
+      {
+        track: {
+          duration_ms: 243769,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 197946,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 180685,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 237253,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 255093,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 125962,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 376490,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 254160,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 226048,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 359214,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 251255,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 261120,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 274333,
+          type: 'track',
+        },
+      },
+      {
+        track: {
+          duration_ms: 141000,
+          type: 'track',
+        },
+      },
+    ],
+    total: 14,
+  },
+  uri: 'spotify:playlist:5Dw0r94mB0oX3hnZqNZUXO',
+};
