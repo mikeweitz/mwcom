25d47796429bd3f42e0f036827a91b34c171d89f
weitzly@gmail.com <w3Itzman>
2023-12-11 18:51:12 -0800

    fix build err

--- pages/spotify/monthly.tsx
+++ pages/spotify/monthly.tsx
@@ -11,7 +11,7 @@ export const getStaticProps = async () => {
     const data = await getPlaylistIds();
     return {
         props: {
-            lists: data.slice(1, data.length),
+            lists: JSON.parse(JSON.stringify(data.slice(1, data.length))),
         },
         revalidate: 1 * 60 * 60 * 24, // value in seconds for 24 hours
     };
@@ -24,12 +24,13 @@ const Monthly = ({ lists }) => {
             <h1>My playlists</h1>
             <section className={styles['playlists-container']}>
                 <aside>
-                    {lists?.map(({ id, name }, n: number) => {
+                    {lists?.map(({ id, name, image }, n: number) => {
                         return (
                             <CardById
                                 key={n}
                                 id={id}
                                 name={name}
+                                image={image}
                                 onClick={() => setSelected(id)}
                             />
                         );
