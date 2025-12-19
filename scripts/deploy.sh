c85836e821b908be4007fd550f70311acfddac64
MW <weitzly@gmail.com>
2025-12-19 12:11:24 -0800

    remove scripts root folder
    legacy from old digital ocean droplet deployment

--- scripts/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Running deployment script..."
-scp -r ~/weitzly/output/html/* "$FTP_USER"@"$REMOTE_IP":"$REMOTE_PATH"/"$BUILD_PATH" && scp_status=$?
-echo SCP returned with code $scp_status
-echo "Pushed deployment successfully"
-exit 0
--- scripts/hello.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-import sys
-
-def main():
-    print("Hello there", sys.argv[1])
-
-if __name__ == '__main__':
-    main()
