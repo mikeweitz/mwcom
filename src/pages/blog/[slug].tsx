9eb0d202c444f1f13ee8c03f2898d073ce1cb7a2
MW <weitzly@gmail.com>
2026-01-31 12:31:41 -0800

    adding container style to blog detail

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -4,18 +4,18 @@ import cx from 'classnames';
 import { useRouter } from 'next/router';
 
 import Layout from '@mw/components/layout';
-import PlaylistDetails from '@mw/components/playlist-details-v3';
+// import PlaylistDetails from '@mw/components/playlist-details-v3';
 // import PlaylistDetailsV2 from '@mw/components/playlist-details-v2';
-import { CardById } from '@mw/components/playlists';
+// import { CardById } from '@mw/components/playlists';
 import { ScrollProvider } from '@mw/components/scrollContext';
-import { Close } from '@mw/components/icons';
-import { getPlaylistFromApi } from '@mw/helpers/sheets';
-import colors from '@mw/constants/colors';
-import Drawer from '@mw/components/drawer';
-import { fetchPlaylist } from '@mw/helpers/fetch-playlist';
-import { emptyList, tracksAdapter } from '@mw/helpers/fetch-playlist';
+// import { Close } from '@mw/components/icons';
+// import { getPlaylistFromApi } from '@mw/helpers/sheets';
+// import colors from '@mw/constants/colors';
+// import Drawer from '@mw/components/drawer';
+// import { fetchPlaylist } from '@mw/helpers/fetch-playlist';
+// import { emptyList, tracksAdapter } from '@mw/helpers/fetch-playlist';
 
-// import styles from './styles.module.scss';
+import styles from './styles.module.scss';
 
 export const getServerSideProps = async ({ query: { slug } }) => {
     const response = await fetch(
@@ -45,7 +45,7 @@ const Playlists = ({ title, content, date }) => {
                 <meta name="description" content="Playlists Details" />
             </Head>
             <Layout>
-                <div>
+                <div className={styles.container}>
                     <h1>{title}</h1>
                     <Date>{new Date(date).toLocaleDateString()}</Date>
                     <div dangerouslySetInnerHTML={{ __html: content }} />
