a619e4ea6ef1de285ec238a0b26b0319080cefcc
MW <weitzly@gmail.com>
2026-02-19 12:41:31 -0800

    make switch to client side

--- src/pages/blog/adjacent-posts.tsx
+++ src/pages/blog/adjacent-posts.tsx
@@ -23,58 +23,58 @@ export default function AdjacentPosts({
 
     useEffect(() => {
         if (inViewport) {
-            console.log('get posts...', url + '/api/wp/get-adjacent-posts');
-            fetch(new URL(url + '/api/wp/get-adjacent-posts'), {
-                method: 'POST',
-                body: JSON.stringify({ date }),
-            })
-                .then((res) => {
-                    if (res.headers.get('content-type').includes('json')) {
-                        return res.json();
-                    } else {
-                        return res.text();
-                    }
-                })
-                .then((result) => {
-                    console.log('received from api:', result);
-                    if (result) {
-                        setPosts(result);
-                    }
-                })
-                .catch((e) => {
-                    console.log('error returned server side', e);
-                });
+            // console.log('get posts...', url + '/api/wp/get-adjacent-posts');
+            // fetch(new URL(url + '/api/wp/get-adjacent-posts'), {
+            //     method: 'POST',
+            //     body: JSON.stringify({ date }),
+            // })
+            //     .then((res) => {
+            //         if (res.headers.get('content-type').includes('json')) {
+            //             return res.json();
+            //         } else {
+            //             return res.text();
+            //         }
+            //     })
+            //     .then((result) => {
+            //         console.log('received from api:', result);
+            //         if (result) {
+            //             setPosts(result);
+            //         }
+            //     })
+            //     .catch((e) => {
+            //         console.log('error returned server side', e);
+            //     });
 
-            // const fields = 'ID,title,slug,date';
-            // Promise.all([
-            //     fetch(
-            //         new URL(
-            //             process.env.NEXT_PUBLIC_WP_API_HOST +
-            //                 `/posts?after=${date}&number=1&fields=${fields}&order=ASC`
-            //         ),
-            //         {
-            //             mode: 'cors',
-            //         }
-            //     ),
-            //     fetch(
-            //         new URL(
-            //             process.env.NEXT_PUBLIC_WP_API_HOST +
-            //                 `/posts?before=${date}&number=1&fields=${fields}`
-            //         ),
-            //         {
-            //             mode: 'cors',
-            //         }
-            //     ),
-            // ])
-            //     .then(([after, before]) => {
-            //         console.log('after before', after, before);
-            //         return Promise.all([after.json(), before.json()]);
-            //     })
-            //     .then(([next, prev]) => {
-            //         console.log('after json called results:', next, prev);
-            //         setPosts({ next: next.posts[0], prev: prev.posts[0] });
-            //     })
-            //     .catch((e) => console.log('error clause', e));
+            const fields = 'ID,title,slug,date';
+            Promise.all([
+                fetch(
+                    new URL(
+                        process.env.NEXT_PUBLIC_WP_API_HOST +
+                            `/posts?after=${date}&number=1&fields=${fields}&order=ASC`
+                    ),
+                    {
+                        mode: 'cors',
+                    }
+                ),
+                fetch(
+                    new URL(
+                        process.env.NEXT_PUBLIC_WP_API_HOST +
+                            `/posts?before=${date}&number=1&fields=${fields}`
+                    ),
+                    {
+                        mode: 'cors',
+                    }
+                ),
+            ])
+                .then(([after, before]) => {
+                    console.log('after before', after, before);
+                    return Promise.all([after.json(), before.json()]);
+                })
+                .then(([next, prev]) => {
+                    console.log('after json called results:', next, prev);
+                    setPosts({ next: next.posts[0], prev: prev.posts[0] });
+                })
+                .catch((e) => console.log('error clause', e));
         }
     }, [date, inViewport]);
 
