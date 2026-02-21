1dc0b127fb759fc93f551bcd5e6d646372ca5966
MW <weitzly@gmail.com>
2026-02-21 09:59:03 -0800

    prelim half working version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,72 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
 import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const compat = new FlatCompat({
     // import.meta.dirname is available after Node.js v20.11.0
-    baseDirectory: import.meta.dirname,
+    baseDirectory: __dirname,
+    recommendedConfigs: {
+        next: 'next',
+        'next/core-web-vitals': 'next/core-web-vitals',
+    },
 });
 
 const eslintConfig = [
-    ...compat.config(
-        {
-            extends: ['next/core-web-vitals', 'next/typescript'],
+    {
+        ignores: [
+            '**/node_modules/**',
+            '.next/**',
+            'dist/**',
+            'coverage/**',
+            '**/*.min.js',
+        ],
+    },
+
+    ...tseslint.configs.recommended,
+
+    // ...compat.extends('next/typescript'),
+    // ...compat.config({
+    //     // extends: ['next/typescript'],
+    // }),
+    // Use compat for legacy next and prettier configs
+    // ...compat.config({
+    //     extends: [
+    //         'next/core-web-vitals', // Includes recommended Next.js, React, and Core Web Vitals rules
+    //         'next/typescript', // Adds Next.js specific TypeScript rules
+    //         // 'prettier', // Integrates Prettier
+    //     ],
+    // }),
+
+    {
+        files: ['**/*.{js,jsx,ts,tsx}'],
+        rules: {
+            // Add or override specific rules here
+            '@typescript-eslint/no-explicit-any': 'warn',
+            '@typescript-eslint/no-unused-vars': 'warn',
         },
-        {
-            ignores: [
-                '**/build/**',
-                '**/out/**',
-                '**/node_modules/**',
-                '**/vendor/**',
-            ],
-        }
-    ),
+    },
+
+    // ...compat
+    //     .config
+    // {
+    //     extends: ['next/typescript'],
+    // },
+    // {
+    //     ignores: [
+    //         '**/build/**',
+    //         '**/out/**',
+    //         '**/node_modules/**',
+    //         '**/vendor/**',
+    //     ],
+    // }
+    // (),
 ];
 
 export default eslintConfig;
