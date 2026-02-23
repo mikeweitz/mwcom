241019eb54924759658c7728e3fe177b86cbd7bb
MW <weitzly@gmail.com>
2026-02-23 15:18:08 -0800

    fixing eslint  to run in repo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,72 +1,28 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
-import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
-
-import { FlatCompat } from '@eslint/eslintrc';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const compat = new FlatCompat({
-    // import.meta.dirname is available after Node.js v20.11.0
-    baseDirectory: __dirname,
-    recommendedConfigs: {
-        next: 'next',
-        'next/core-web-vitals': 'next/core-web-vitals',
-    },
-});
-
-const eslintConfig = [
-    {
-        ignores: [
-            '**/node_modules/**',
-            '.next/**',
-            'dist/**',
-            'coverage/**',
-            '**/*.min.js',
-        ],
-    },
-
-    ...tseslint.configs.recommended,
-
-    // ...compat.extends('next/typescript'),
-    // ...compat.config({
-    //     // extends: ['next/typescript'],
-    // }),
-    // Use compat for legacy next and prettier configs
-    // ...compat.config({
-    //     extends: [
-    //         'next/core-web-vitals', // Includes recommended Next.js, React, and Core Web Vitals rules
-    //         'next/typescript', // Adds Next.js specific TypeScript rules
-    //         // 'prettier', // Integrates Prettier
-    //     ],
-    // }),
-
+const eslintConfig = defineConfig([
+    ...nextVitals,
+    ...nextTs,
+    eslintConfigPrettier,
+    // Override default ignores of eslint-config-next.
+    globalIgnores([
+        // Default ignores of eslint-config-next:
+        '.next/**',
+        'out/**',
+        'build/**',
+        'next-env.d.ts',
+    ]),
     {
         files: ['**/*.{js,jsx,ts,tsx}'],
         rules: {
-            // Add or override specific rules here
+            'react/no-unescaped-entities': 'off',
             '@typescript-eslint/no-explicit-any': 'warn',
             '@typescript-eslint/no-unused-vars': 'warn',
         },
     },
-
-    // ...compat
-    //     .config
-    // {
-    //     extends: ['next/typescript'],
-    // },
-    // {
-    //     ignores: [
-    //         '**/build/**',
-    //         '**/out/**',
-    //         '**/node_modules/**',
-    //         '**/vendor/**',
-    //     ],
-    // }
-    // (),
-];
+]);
 
 export default eslintConfig;
