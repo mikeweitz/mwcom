eb085ca77a067e8f0309b8de49a4f5d7a6e027e1
MW <weitzly@gmail.com>
2026-02-23 17:40:49 -0800

    remove old calls to data files

--- src/pages/api/spotify.js
+++ src/pages/api/spotify.js
@@ -1,17 +1,17 @@
-import { playlists } from './data';
+// import { playlists } from './data';
 
-export default (req, res) => {
-  // script data down to just IDs
-  const pids = playlists.map((list) => ({
-    pid: list.id || list.playlistV2.uri.split(':').pop(),
-    date: list.date,
-  }));
-  // PL with id exists
-  if (pids.length > 0) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json(pids);
-  } else {
-    res.status(404).json({ message: `data not found` });
-  }
-};
+// export default (req, res) => {
+//   // script data down to just IDs
+//   const pids = playlists.map((list) => ({
+//     pid: list.id || list.playlistV2.uri.split(':').pop(),
+//     date: list.date,
+//   }));
+//   // PL with id exists
+//   if (pids.length > 0) {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'application/json');
+//     res.json(pids);
+//   } else {
+//     res.status(404).json({ message: `data not found` });
+//   }
+// };
