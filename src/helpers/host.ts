22276e18e6e5606843c03ba0a134f1a37e860a62
MW <weitzly@gmail.com>
2026-02-24 10:50:05 -0800

    forgot to prefix host w protocol

--- src/helpers/host.ts
+++ src/helpers/host.ts
@@ -1,6 +1,6 @@
 export const HOST =
     process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
-        ? process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
+        ? 'https://' + process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
         : process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview'
-          ? process.env.VERCEL_URL
+          ? 'https://' + process.env.VERCEL_URL
           : 'http://localhost:3000';
