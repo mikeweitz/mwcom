1c5ca1528c10b66ebfad1ac8d694a5fb1bdd4ba2
MW <weitzly@gmail.com>
2026-08-20 21:01:17 -0700

    need new refresh token

--- src/pages/spotify/callback.tsx
+++ src/pages/spotify/callback.tsx
@@ -1,8 +1,55 @@
 // callback URL for spotify app auth
-const Callback = () => (
-    <main>
-        <h1 style={{ color: 'white' }}>Spotify callback url</h1>
-    </main>
-);
+
+export async function getServerSideProps({ req, res }) {
+    const code = req.query.code;
+    // const response = await fetch('https://accounts.spotify.com/api/token', {
+    //     method: 'POST',
+    //     headers: {
+    //         Authorization: `Basic ${Buffer.from(
+    //             `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
+    //         ).toString('base64')}`,
+    //         'Content-Type': 'application/x-www-form-urlencoded',
+    //     },
+    //     body: new URLSearchParams({
+    //         grant_type: 'refresh_token',
+    //         refresh_token,
+    //     }),
+    // });
+    // const data = await response.json();
+    // console.log('Spotify access token', data);
+    // return data;
+
+    const tokenRes = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            Authorization: `Basic ${Buffer.from(
+                `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
+            ).toString('base64')}`,
+        },
+        body: new URLSearchParams({
+            code,
+            redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
+            grant_type: 'authorization_code',
+        }),
+    });
+    const tokenData = await tokenRes.json();
+
+    const { access_token, refresh_token } = tokenData;
+    // console.log('Spotify access token', tokenData);
+    return {
+        props: { access_token, refresh_token },
+    };
+}
+
+const Callback = ({ access_token, refresh_token }) => {
+    return (
+        <main>
+            <h1 style={{ color: 'white' }}>Spotify callback url</h1>
+            {access_token && <p>Access Token: {access_token}</p>}
+            {refresh_token && <p>Refresh Token: {refresh_token}</p>}
+        </main>
+    );
+};
 
 export default Callback;
---
+++ src/pages/spotify/login.tsx
@@ -0,0 +1,103 @@
+const client_id = process.env.SPOTIFY_CLIENT_ID;
+const redirect_uri = process.env.SPOTIFY_REDIRECT_URI; // 'http://127.0.0.1:8888/callback';
+
+// var app = express();
+
+// app.get('/login', function(req, res) {
+
+//   var state = generateRandomString(16);
+//   var scope = 'user-read-private user-read-email';
+
+//   res.redirect('https://accounts.spotify.com/authorize?' +
+//     querystring.stringify({
+//       response_type: 'code',
+//       client_id: client_id,
+//       scope: scope,
+//       redirect_uri: redirect_uri,
+//       state: state
+//     }));
+// });
+
+import { useEffect, useState } from 'react';
+
+import { topTracks } from '@mw/helpers/spotify';
+
+type Track = {
+    title: string;
+    artist: string;
+    url: string;
+    coverImage: Record<string, any>;
+};
+
+type Tracks = Track[];
+
+// export const getServerSideProps = async () => {
+//     const response = await topTracks();
+//     const tracks: Tracks = [];
+//     console.log('retrieve top tracks', response);
+//     if (response.ok) {
+//         const { items } = await response.json();
+//         for (const { name, artists, external_urls, album } of items) {
+//             tracks.push({
+//                 title: name,
+//                 artist: artists.map((_artist) => _artist.name).join(', '),
+//                 url: external_urls.spotify,
+//                 coverImage: album.images[1],
+//             });
+//         }
+//     }
+//     return { props: { tracks } };
+// };
+
+const fetcher = async () => {
+    const response = await topTracks();
+    const tracks: Tracks = [];
+    console.log('retrieve top tracks', response);
+    if (response.ok) {
+        const { items } = await response.json();
+        for (const { name, artists, external_urls, album } of items) {
+            tracks.push({
+                title: name,
+                artist: artists.map((_artist) => _artist.name).join(', '),
+                url: external_urls.spotify,
+                coverImage: album.images[1],
+            });
+        }
+    }
+    return { props: { tracks } };
+};
+
+const Login = () => {
+    // convert this to a server side function that returns the url to redirect to spotify login
+    //   var state = generateRandomString(16);
+    //   var scope = 'user-read-private user-read-email';
+
+    //   res.redirect('https://accounts.spotify.com/authorize?' +
+    //     querystring.stringify({
+    //       response_type: 'code',
+    //       client_id: client_id,
+    //       scope: scope,
+    //       redirect_uri: redirect_uri,
+    //       state: state
+    //     }));
+    //
+    const spotifyLoginUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=user-read-private%20user-read-email&redirect_uri=${redirect_uri}`;
+
+    const [tracks, setTracks] = useState<Tracks>();
+
+    useEffect(() => {
+        // window.location.href = spotifyLoginUrl;
+        // fetcher()
+        //     .then(({ props }) => setTracks(props.tracks))
+        //     .catch((e) => console.error('could not fetch', e));
+    }, []);
+
+    return (
+        <main>
+            <h1>Login redirect</h1>
+            <a href={spotifyLoginUrl}>Login with Spotify</a>
+        </main>
+    );
+};
+
+export default Login;
